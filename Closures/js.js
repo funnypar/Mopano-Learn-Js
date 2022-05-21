c908e54623cab5163bbf8cd956722b29630eac95
Mopano <99732517+funnypar@users.noreply.github.com>
2022-05-21 14:01:59 +0430

    Add files via upload

--- Closures/js.js
+++ Closures/js.js
@@ -15,4 +15,26 @@ func();
 func();
 
 console.dir(func); // With this method of console we can see the properties of functions !!!
+
 // More examples of closures :
+
+let f;
+
+const firstFunc = function () {
+  const a = 55;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const secondFunc = function () {
+  const b = 100;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+firstFunc();
+f();
+secondFunc();
+f();
