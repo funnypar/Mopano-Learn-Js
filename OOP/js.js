a48c010212e1a474d487fb9869ed99f65080d47a
Mopano <99732517+funnypar@users.noreply.github.com>
2022-06-27 00:50:56 +0430

    Add methods to prototype

--- OOP/js.js
+++ OOP/js.js
@@ -8,3 +8,15 @@ const Person = function (firstName, birthYear) {
 
 const mopano = new Person("parsa", 2000);
 console.log(mopano);
+
+// Add methods ( prototype:) )
+Person.prototype.calcAge = function () {
+  console.log(`${this.firstName} is ${2022 - this.birthYear} years old :)`);
+};
+
+mopano.calcAge();
+
+//Add property to prototype
+Person.prototype.language = "Persich";
+
+console.log(mopano.language);
