1e39f8faade25998517041e0d511579aa42c2f8c
Mopano <99732517+funnypar@users.noreply.github.com>
2022-06-27 01:21:55 +0430

    Added view of prototype chain and added methods to prototype of Array

--- OOP/js.js
+++ OOP/js.js
@@ -20,3 +20,18 @@ mopano.calcAge();
 Person.prototype.language = "Persich";
 
 console.log(mopano.language);
+
+//Prototype chain
+console.log(mopano.__proto__);
+console.log(mopano.__proto__.__proto__);
+console.log(mopano.__proto__.__proto__.__proto__);
+
+//You can add some methods to prototype of Arrays or ...
+console.log(Array.prototype);
+Array.prototype.unique = function () {
+  // You added method in array prototype
+  return [...new Set(this)];
+};
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11];
+console.log(arr.unique());
