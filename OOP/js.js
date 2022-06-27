f3731c5ee696b720a1f6905f3a320d5326d668b9
Mopano <99732517+funnypar@users.noreply.github.com>
2022-06-27 18:01:35 +0430

    Added ES6 classes

--- OOP/js.js
+++ OOP/js.js
@@ -1,37 +1,51 @@
 "use strict";
 
 //OOP constructor functions
-const Person = function (firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
-};
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+// };
 
-const mopano = new Person("parsa", 2000);
-console.log(mopano);
+// const mopano = new Person("parsa", 2000);
+// console.log(mopano);
 
 // Add methods ( prototype:) )
-Person.prototype.calcAge = function () {
-  console.log(`${this.firstName} is ${2022 - this.birthYear} years old :)`);
-};
+// Person.prototype.calcAge = function () {
+//   console.log(`${this.firstName} is ${2022 - this.birthYear} years old :)`);
+// };
 
-mopano.calcAge();
+// mopano.calcAge();
 
 //Add property to prototype
-Person.prototype.language = "Persich";
+// Person.prototype.language = "Persich";
 
-console.log(mopano.language);
+// console.log(mopano.language);
 
 //Prototype chain
-console.log(mopano.__proto__);
-console.log(mopano.__proto__.__proto__);
-console.log(mopano.__proto__.__proto__.__proto__);
+// console.log(mopano.__proto__);
+// console.log(mopano.__proto__.__proto__);
+// console.log(mopano.__proto__.__proto__.__proto__);
 
 //You can add some methods to prototype of Arrays or ...
-console.log(Array.prototype);
-Array.prototype.unique = function () {
-  // You added method in array prototype
-  return [...new Set(this)];
-};
+// console.log(Array.prototype);
+// Array.prototype.unique = function () {
+// You added method in array prototype
+//   return [...new Set(this)];
+// };
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11];
-console.log(arr.unique());
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11];
+// console.log(arr.unique());
+
+//ES6 classes
+class Person {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(`${this.firstName} is ${2022 - this.birthYear} years old :) `);
+  }
+}
+const mopano = new Person("mopano", 2000);
+mopano.calcAge();
